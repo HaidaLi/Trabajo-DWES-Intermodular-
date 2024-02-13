6d89499a5f37368f2ef0d13580b1bb2360591688
HaidaLi <lihaidali@gmail.com>
2024-02-13 20:30:05 +0100

    nuevos cambios

--- Intermodular/app/Models/Token.php
+++ Intermodular/app/Models/Token.php
@@ -5,15 +5,16 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use App\Models\Categoria;
+use Database\Seeders\Formulario;
 
-class Servicio extends Model
+class Token extends Model
 {
     use HasFactory;
 
     protected $table =  'servicios';
 
-    public  function categorias(){
-        return $this->belongsToMany(Categoria::class);
+    public  function empresa(){
+        return $this->belongsTo(Formulario::class);
     }
 
 }
---
+++ Intermodular/database/factories/formularioFactory.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Model>
+ */
+class formularioFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'nombre' => $this->faker->sentence,
+            'descripcion' => $this->faker->text,
+            'tipo' => $this->faker->sentence(2)
+        ];
+    }
+}
